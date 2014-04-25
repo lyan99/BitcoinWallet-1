0cc2751a4f7eafe544207e37f45c9a341fd53aaa
ScripterRon <Ronald.Hoffman6@gmail.com>
2014-04-25 12:30:44 -0400

    Add hh:mm to date display
    Display date as 'mm/dd/yyyy hh:mm'

--- src/main/java/org/ScripterRon/BitcoinWallet/DateRenderer.java
+++ src/main/java/org/ScripterRon/BitcoinWallet/DateRenderer.java
@@ -54,9 +54,11 @@ public final class DateRenderer extends DefaultTableCellRenderer {
         if (!(value instanceof Date))
             throw new IllegalArgumentException("Value is not a Date");
         cal.setTime((Date)value);
-        setText(String.format("%02d/%02d/%04d",
+        setText(String.format("%02d/%02d/%04d %02d:%02d",
                               cal.get(Calendar.MONTH)+1,
                               cal.get(Calendar.DAY_OF_MONTH),
-                              cal.get(Calendar.YEAR)));
+                              cal.get(Calendar.YEAR),
+                              cal.get(Calendar.HOUR_OF_DAY),
+                              cal.get(Calendar.MINUTE)));
     }
 }
