cf0dfa15eabe9aecc1ed77802c170ba619f838d4
ScripterRon <Ronald.Hoffman6@gmail.com>
2018-05-26 20:46:28 -0400

    Update packaging

--- package.sh
+++ package.sh
@@ -15,22 +15,20 @@ if [ ! -f target/BitcoinWallet-$VERSION.jar ] ; then
   exit 1
 fi
 
-cd package
-rm lib/*
-cp ../LICENSE ../README.md ../sample.BitcoinWallet.conf ../sample.logging.properties .
-cp ../target/BitcoinWallet-$VERSION.jar .
-cp ../target/lib/* lib
-
-if [ -f BitcoinWallet-$VERSION.zip ] ; then
-  rm BitcoinWallet-$VERSION.zip
+if [ ! -d package ] ; then
+  mkdir package
+else
+  rm -r package/*
 fi
-zip -r BitcoinWallet-$VERSION.zip BitcoinWallet-$VERSION.jar lib LICENSE README.md sample.BitcoinWallet.conf sample.logging.properties
+
+cd package
+cp ../ChangeLog.txt ../LICENSE ../README.md ../sample.BitcoinWallet.conf ../sample.logging.properties .
+cp -r ../target/BitcoinWallet-$VERSION.jar ../target/lib .
+
+zip -r BitcoinWallet-$VERSION.zip BitcoinWallet-$VERSION.jar lib ChangeLog.txt LICENSE README.md sample.BitcoinWallet.conf sample.logging.properties
 echo "Created BitcoinWallet-$VERSION.zip"
 
-if [ -f BitcoinWallet-$VERSION.tar.gz ] ; then
-  rm BitcoinWallet-$VERSION.tar.gz
-fi
-tar zchf BitcoinWallet-$VERSION.tar.gz BitcoinWallet-$VERSION.jar lib LICENSE README.md sample.BitcoinWallet.conf sample.logging.properties
+tar zchf BitcoinWallet-$VERSION.tar.gz BitcoinWallet-$VERSION.jar lib ChangeLog.txt LICENSE README.md sample.BitcoinWallet.conf sample.logging.properties
 echo "Created BitcoinWallet-$VERSION.tar.gz"
 exit 0
 
